856ff32fb3b0e88b11b7e5d64409a48f736ad6ab
Christian Roese <christian.roese@mortonbuildings.com>
2020-12-29 14:26:14 -0600

    adds infrastructure for verbs/adverbs/additional config options

--- index.js
+++ index.js
@@ -8,33 +8,43 @@ var randy = require('randy');
 function Phraseit() {
   var self = this;
 
-  self._nouns      = require('./words/nouns.js');
+  self._nouns = require('./words/nouns.js');
   self._adjectives = require('./words/adjectives.js');
+  self._adverbs = require('./words/adverbs.js');
+  self._verbs = require('./words/verbs.js');
 
   self.actions = {
-    noun: function() {
+    noun: function () {
       return randy.choice(self._nouns);
     },
-    a_noun: function() {
-      return articles.articlize( self.actions.noun() );
+    a_noun: function () {
+      return articles.articlize(self.actions.noun());
     },
-    adjective: function() {
+    adjective: function () {
       return randy.choice(self._adjectives);
     },
-    an_adjective: function() {
-      return articles.articlize( self.actions.adjective() );
-    }
+    an_adjective: function () {
+      return articles.articlize(self.actions.adjective());
+    },
+    adverb: function () {
+      return randy.choice(self._adverbs);
+    },
+    verb: function () {
+      return randy.choice(self._verbs);
+    },
   };
 
-  self.configure = function(options) {
+  self.configure = function (options) {
     // merge actions
     Object.assign(self.actions, options.actions || {});
-    // overwrite nouns and adjectives if we got some
-    self._nouns      = options.nounList || self._nouns;
-    self._adjectives = options.adjectiveList || self._adjectives;
+
+    self._nouns = (options.nounList || self._nouns).concat(options.addNouns || []);
+    self._adjectives = (options.adjectiveList || self._adjectives).concat(options.addAdjectives || []);
+    self._adverbs = (options.adverbList || self._adverbs).concat(options.addAdverbs || []);
+    self._verbs = (options.verbList || self._verbs).concat(options.addVerbs || []);
   };
 
-  self.use = function(options) {
+  self.use = function (options) {
     var newInstance = new Phraseit();
     newInstance.configure(options);
     return newInstance;
@@ -45,23 +55,23 @@ function Phraseit() {
 //                  THE GOODS
 // ---------------------------------------------
 
-Phraseit.prototype.make = function(template) {
+Phraseit.prototype.make = function (template) {
   var self = this;
 
   var sentence = template;
   var occurrences = template.match(/\{\{(.+?)\}\}/g);
 
-  if(occurrences && occurrences.length) {
-    for(var i = 0; i < occurrences.length; i++) {
+  if (occurrences && occurrences.length) {
+    for (var i = 0; i < occurrences.length; i++) {
       var action = occurrences[i].replace('{{', '').replace('}}', '').trim();
       var result = '';
-      if(action.match(/\((.+?)\)/)) {
+      if (action.match(/\((.+?)\)/)) {
         try {
           result = eval('self.actions.' + action);
         }
-        catch(e) { }
+        catch (e) { }
       } else {
-        if(self.actions[action]) {
+        if (self.actions[action]) {
           result = self.actions[action]();
         } else {
           result = '{{ ' + action + ' }}';
